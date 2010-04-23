98ad0ce832f8f2aaa22168f1db4ae466e80f3028
scott@scottjehl.com <scott@scottjehl.com@abc99cd0-1bda-11df-80bc-e1127a2a1d63>
2010-04-23 22:19:06 +0000

    Added a new feature for charting subsets of a data table, addressing issue #3.
    Two new options: 
    rowFilter
    colFilter
    
    Both of these accept jQuery selector strings to filter rows and columns by any selectors you'd like. The :eq(), :gt(), and :lt() selectors will probably be most useful, but any selectors are accepted.
    
    
    Some examples...
    
    //chart all columns but the last (useful if you have a totals column)
    $('table').visualize({
    	colFilter: ':not(:last-child)'
    });
    
    //chart all columns whose cells do not have a class of "total"
    $('table').visualize({
    	colFilter: ':not(.total)'
    });
    
    //chart only columns greater than 1 and less than 3
    $('table').visualize({
    	colFilter: ':gt(1):lt(3)'
    });
    
    //chart only the first row of the table
    $('table').visualize({
    	rowFilter: ':eq(0)'
    });

--- charting/js/visualize.jQuery.js
+++ charting/js/visualize.jQuery.js
@@ -17,6 +17,8 @@ $.fn.visualize = function(options, container){
 			appendTitle: true, //table caption text is added to chart
 			title: null, //grabs from table caption if null
 			appendKey: true, //color key is added to chart
+			rowFilter: ' ',
+			colFilter: ' ',
 			colors: ['#be1e2d','#666699','#92d5ea','#ee8310','#8d10ee','#5a3b16','#26a4ed','#f45a90','#e9e744'],
 			textColors: [], //corresponds with colors array. null/undefined items will fall back to CSS
 			parseDirection: 'x', //which direction to parse the table data
@@ -44,25 +46,25 @@ $.fn.visualize = function(options, container){
 				dataGroups: function(){
 					var dataGroups = [];
 					if(o.parseDirection == 'x'){
-						self.find('tr:gt(0)').each(function(i){
+						self.find('tr:gt(0)').filter(o.rowFilter).each(function(i){
 							dataGroups[i] = {};
 							dataGroups[i].points = [];
 							dataGroups[i].color = colors[i];
 							if(textColors[i]){ dataGroups[i].textColor = textColors[i]; }
-							$(this).find('td').each(function(){
+							$(this).find('td').filter(o.colFilter).each(function(){
 								dataGroups[i].points.push( parseFloat($(this).text()) );
 							});
 						});
 					}
 					else {
-						var cols = self.find('tr:eq(1) td').size();
+						var cols = self.find('tr:eq(1) td').filter(o.colFilter).size();
 						for(var i=0; i<cols; i++){
 							dataGroups[i] = {};
 							dataGroups[i].points = [];
 							dataGroups[i].color = colors[i];
 							if(textColors[i]){ dataGroups[i].textColor = textColors[i]; }
-							self.find('tr:gt(0)').each(function(){
-								dataGroups[i].points.push( $(this).find('td').eq(i).text()*1 );
+							self.find('tr:gt(0)').filter(o.rowFilter).each(function(){
+								dataGroups[i].points.push( $(this).find('td').filter(o.colFilter).eq(i).text()*1 );
 							});
 						};
 					}
@@ -144,12 +146,12 @@ $.fn.visualize = function(options, container){
 				xLabels: function(){
 					var xLabels = [];
 					if(o.parseDirection == 'x'){
-						self.find('tr:eq(0) th').each(function(){
+						self.find('tr:eq(0) th').filter(o.colFilter).each(function(){
 							xLabels.push($(this).html());
 						});
 					}
 					else {
-						self.find('tr:gt(0) th').each(function(){
+						self.find('tr:gt(0) th').filter(o.rowFilter).each(function(){
 							xLabels.push($(this).html());
 						});
 					}
@@ -420,8 +422,15 @@ $.fn.visualize = function(options, container){
 		//append key
 		if(o.appendKey){
 			var newKey = $('<ul class="visualize-key"></ul>');
-			var selector = (o.parseDirection == 'x') ? 'tr:gt(0) th' : 'tr:eq(0) th' ;
-			self.find(selector).each(function(i){
+			var selector;
+			if(o.parseDirection == 'x'){
+				selector = self.find('tr:gt(0) th').filter(o.rowFilter);
+			}
+			else{
+				selector = self.find('tr:eq(0) th').filter(o.colFilter);
+			}
+			
+			selector.each(function(i){
 				$('<li><span class="visualize-key-color" style="background: '+dataGroups[i].color+'"></span><span class="visualize-key-label">'+ $(this).text() +'</span></li>')
 					.appendTo(newKey);
 			});
